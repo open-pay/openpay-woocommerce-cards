300aaf48dfb16a32f830228a1f7be643dbb5c70f
DEV-JOSE-ROMERO <jose.romero@openpay.mx>
2024-09-12 18:47:19 -0600

    UnitTest UtilsTest

---
+++ tests/UtilsTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class UtilsTest extends TestCase {
+    public function testGetCountryName() {
+        $condition = true;
+        $this->assertTrue($condition);
+    }
+}
