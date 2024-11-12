e0c4e054bf378ea5b55d98e7be62f5dc2e9826c2
JOSE-ROMERO <jose.romero@JOSE-ROMERO.local>
2024-11-12 14:03:25 -0600

    Coverage configurations

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -2,9 +2,53 @@
 
 use PHPUnit\Framework\TestCase;
 
+require_once 'utils/utils.php';
+
+/**
+ * @covers Utils
+ */
 final class UtilsTest extends TestCase {
-    public function testGetCountryName() {
-        $condition = true;
-        $this->assertTrue($condition);
+
+    private $utilsClass;
+
+    public function setUp(): void
+    {
+        $this->utilsClass = new Utils();
+    }
+
+    public function testGetCountryNameForMx() {
+        $countryCode = 'MX';
+        $expectedResult = 'Mexico';
+
+        $result = $this->utilsClass->getCountryName($countryCode);
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function testGetCountryNameForCo() {
+        $countryCode = 'CO';
+        $expectedResult = 'Colombia';
+
+        $result = $this->utilsClass->getCountryName($countryCode);
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function testGetCountryNameForPe() {
+        $countryCode = 'PE';
+        $expectedResult = 'Peru';
+
+        $result = $this->utilsClass->getCountryName($countryCode);
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function testGetCountryNameForDefault() {
+        $countryCode = 'ARG';
+        $expectedResult = '';
+
+        $result = $this->utilsClass->getCountryName($countryCode);
+
+        $this->assertEquals($expectedResult, $result);
     }
 }
