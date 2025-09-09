016d06098ef312c3bd39af307accbe6dcc72dd4f
AntonioSilvanOpy <joseantonio.silvan@openpay.mx>
2025-09-09 13:24:16 -0600

    Se agrega mappeo de errores para plugin woocommerce

--- includes/OpenpayErrorHandler.php
+++ includes/OpenpayErrorHandler.php
@@ -7,6 +7,7 @@ use Openpay\Data\OpenpayApiRequestError;
 use Openpay\Data\OpenpayApiConnectionError;
 use Openpay\Data\OpenpayApiAuthError;
 use Openpay\Data\OpenpayApiTransactionError;
+use OpenpayCards\Includes\OpenpayErrorManager;
 
 class OpenpayErrorHandler {
     protected static $logger;
@@ -20,7 +21,7 @@ class OpenpayErrorHandler {
         self::$logger = wc_get_logger();
         $context = is_array($context) ? $context : [];
         if (self::$logger) {
-            self::$logger->error($message, array_merge(['source' => 'plugin-openpay-woocommerce-cards'], $context));
+            self::$logger->error($message, $context);
         }
     }
 
@@ -31,30 +32,58 @@ class OpenpayErrorHandler {
         return false; // Deja que PHP siga su curso normal si no quieres suprimir el error
     }*/
 
-    public static function handleOpenpayPluginException($exception) {
+    public static function handleOpenpayPluginException($exception, $order_id, $customer_id = null) {
         if ($exception instanceof OpenpayApiTransactionError || $exception instanceof OpenpayApiError || $exception instanceof OpenpayApiConnectionError ) {
-            $message = "[Openpay ERROR] " . $exception->getMessage();
+            $openpayErrorManager = new OpenpayErrorManager();
+            $errorMessage = $openpayErrorManager::getErrorMessages($exception->getCode());
+            $message = "[Openpay ERROR] " . $errorMessage['logError'];
+            $uuid = self::generate_uuid_v4();
+             $context = [
+                'id' => $uuid,
+                'order_id' => $order_id,
+                'code' => $exception->getCode(),
+                'user_id' => ($customer_id === null) ? 'guest' : $customer_id,
+                'gateway' => 'wc_openpay_cards',
+            ];
+
+            $order = wc_get_order( $order_id );
+            $order->add_order_note( $errorMessage['orderDetailError'] );
+            $order->update_status('failed');
         } else {
             $message = "[EXCEPTION] " . $exception->getMessage();
         }
 
-        self::log($message, ['trace' => $exception->getTraceAsString(),]);
+       
+
+        self::log($message, $context);
     }
 
-    public static function catchOpenpayError($callback) {
-        $log = wc_get_logger();
+    public static function catchOpenpayError($callback, $order_id, $customer_id = null) {
         try {
             return $callback();
         } catch (OpenpayApiConnectionError $e) {
-            self::handleOpenpayPluginException($e);
-            throw new Exception("El servidor no responde", $e->getCode());  // ** SE CREA UNA EXCEPCION PERSONALIZADA PRESERVANDO EL CODE DE LA EXCEPCION ORIGINAL
+            self::handleOpenpayPluginException($e, $order_id, $customer_id);
+            $openpayErrorManager = new OpenpayErrorManager();
+            $errorMessage = $openpayErrorManager::getErrorMessages($e->getCode());
+            throw new Exception($errorMessage['clientError'], $e->getCode());  // ** SE CREA UNA EXCEPCION PERSONALIZADA PRESERVANDO EL CODE DE LA EXCEPCION ORIGINAL
         } catch (OpenpayApiTransactionError $e) {
-            self::handleOpenpayPluginException($e);
-            throw new Exception("Tu tarjeta fue rechazada. Por favor intenta con otra.", $e->getCode());
+            self::handleOpenpayPluginException($e, $order_id, $customer_id);
+             $openpayErrorManager = new OpenpayErrorManager();
+            $errorMessage = $openpayErrorManager::getErrorMessages($e->getCode());
+            throw new Exception($errorMessage['clientError'], $e->getCode());
         } catch (OpenpayApiError $e) {
-            self::handleOpenpayPluginException($e);
-            throw new Exception("El pago no fue exitoso por un error en el sistema, puedes intentar de nuevo.", $e->getCode());
+            self::handleOpenpayPluginException($e, $order_id, $customer_id);
+            $openpayErrorManager = new OpenpayErrorManager();
+            $errorMessage = $openpayErrorManager::getErrorMessages($e->getCode());
+            throw new Exception($errorMessage['clientError'], $e->getCode());
         }
         return false;
+    }
+
+    public static function generate_uuid_v4() {
+        $data = random_bytes(16);
+        $data[6] = chr((ord($data[6]) & 0x0f) | 0x40);
+        $data[8] = chr((ord($data[8]) & 0x3f) | 0x80);
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 }
