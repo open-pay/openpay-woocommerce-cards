b4ad1d8b16ab53f1fadc9b289a371b7946851749
Marcos Vazquez Gonzalez <marcos.vazquez@openpay.mx>
2026-06-17 11:38:20 -0600

    Se habilita el campo check Impoconsumo en admin para Colombia Se agrega campo Impoconsumo en producto Colombia Se agrega el desglose de información con impoconsumo de los productos

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,70 +1,101 @@
 jQuery(document).ready(function () {
-    var country = jQuery('#woocommerce_wc_openpay_gateway_country').val();
-    showOrHideElements(country)
+  var country = jQuery("#woocommerce_wc_openpay_gateway_country").val();
+  showOrHideElements(country);
 
-    jQuery('#woocommerce_wc_openpay_gateway_country').change(function () {
-        var country = jQuery(this).val();
-        console.log('woocommerce_wc_openpay_gateway_country', country);        
+  jQuery("#woocommerce_wc_openpay_gateway_country").change(function () {
+    var country = jQuery(this).val();
+    console.log("woocommerce_wc_openpay_gateway_country", country);
 
-        showOrHideElements(country)
-    });
+    showOrHideElements(country);
+  });
 
-    function showOrHideElements (country){
-
-        var chargeMX = jQuery('#woocommerce_wc_openpay_gateway_charge_type').closest('tr');
-        var chargeCOPE = jQuery('#woocommerce_wc_openpay_gateway_charge_type_co_pe').closest('tr');
-        if (country === 'MX'|| country === undefined) {
-            chargeMX.show();
-            chargeCOPE.hide();
-        } else {
-            chargeMX.hide();
-            chargeCOPE.show();
-        }
-
-
-        if(country == 'PE' || country == 'CO'){
-            jQuery("#woocommerce_wc_openpay_gateway_card_points").closest("tr").hide();
-            jQuery("#woocommerce_wc_openpay_gateway_msi").closest("tr").hide();
-            jQuery("#woocommerce_wc_openpay_gateway_minimum_amount_interest_free").closest("tr").hide();
-            jQuery("#woocommerce_wc_openpay_gateway_installments_is_active").closest("tr").show();
-            if(country == 'PE') {
-                jQuery("#woocommerce_wc_openpay_gateway_save_card_mode option[value='2']").show();
-                jQuery("#woocommerce_wc_openpay_gateway_capture").closest("tr").show();
-                jQuery("#woocommerce_wc_openpay_gateway_iva").closest("tr").hide();
-            }
-            if(country == 'CO') {
-                jQuery("#woocommerce_wc_openpay_gateway_save_card_mode option[value='2']").hide();
-                jQuery("#woocommerce_wc_openpay_gateway_capture").closest("tr").hide();
-                jQuery("#woocommerce_wc_openpay_gateway_iva").closest("tr").show();
-            }
-        } else if (country == 'MX'){
-            jQuery("#woocommerce_wc_openpay_gateway_save_card_mode option[value='2']").hide();
-            jQuery("#woocommerce_wc_openpay_gateway_installments_is_active").closest("tr").hide();
-            jQuery("#woocommerce_wc_openpay_gateway_iva").closest("tr").hide();
-            jQuery("#woocommerce_wc_openpay_gateway_capture").closest("tr").show();
-            jQuery("#woocommerce_wc_openpay_gateway_card_points").closest("tr").show();
-            jQuery("#woocommerce_wc_openpay_gateway_msi").closest("tr").show();
-            jQuery("#woocommerce_wc_openpay_gateway_minimum_amount_interest_free").closest("tr").show();
-        }
+  function showOrHideElements(country) {
+    var chargeMX = jQuery(
+      "#woocommerce_wc_openpay_gateway_charge_type",
+    ).closest("tr");
+    var chargeCOPE = jQuery(
+      "#woocommerce_wc_openpay_gateway_charge_type_co_pe",
+    ).closest("tr");
+    if (country === "MX" || country === undefined) {
+      chargeMX.show();
+      chargeCOPE.hide();
+    } else {
+      chargeMX.hide();
+      chargeCOPE.show();
     }
-    
 
-    if(jQuery("#woocommerce_wc_openpay_gateway_sandbox").length){
+    if (country == "PE" || country == "CO") {
+      jQuery("#woocommerce_wc_openpay_gateway_card_points")
+        .closest("tr")
+        .hide();
+      jQuery("#woocommerce_wc_openpay_gateway_msi").closest("tr").hide();
+      jQuery("#woocommerce_wc_openpay_gateway_minimum_amount_interest_free")
+        .closest("tr")
+        .hide();
+      jQuery("#woocommerce_wc_openpay_gateway_installments_is_active")
+        .closest("tr")
+        .show();
+      if (country == "PE") {
+        jQuery(
+          "#woocommerce_wc_openpay_gateway_save_card_mode option[value='2']",
+        ).show();
+        jQuery("#woocommerce_wc_openpay_gateway_capture").closest("tr").show();
+        jQuery("#woocommerce_wc_openpay_gateway_iva").closest("tr").hide();
+        jQuery("#woocommerce_wc_openpay_gateway_impoconsumo")
+          .closest("tr")
+          .hide();
+      }
+      if (country == "CO") {
+        jQuery(
+          "#woocommerce_wc_openpay_gateway_save_card_mode option[value='2']",
+        ).hide();
+        jQuery("#woocommerce_wc_openpay_gateway_capture").closest("tr").hide();
+        jQuery("#woocommerce_wc_openpay_gateway_iva").closest("tr").show();
+        jQuery("#woocommerce_wc_openpay_gateway_impoconsumo")
+          .closest("tr")
+          .show();
+      }
+    } else if (country == "MX") {
+      jQuery(
+        "#woocommerce_wc_openpay_gateway_save_card_mode option[value='2']",
+      ).hide();
+      jQuery("#woocommerce_wc_openpay_gateway_installments_is_active")
+        .closest("tr")
+        .hide();
+      jQuery("#woocommerce_wc_openpay_gateway_iva").closest("tr").hide();
+      jQuery("#woocommerce_wc_openpay_gateway_impoconsumo")
+        .closest("tr")
+        .hide();
+      jQuery("#woocommerce_wc_openpay_gateway_capture").closest("tr").show();
+      jQuery("#woocommerce_wc_openpay_gateway_card_points")
+        .closest("tr")
+        .show();
+      jQuery("#woocommerce_wc_openpay_gateway_msi").closest("tr").show();
+      jQuery("#woocommerce_wc_openpay_gateway_minimum_amount_interest_free")
+        .closest("tr")
+        .show();
+    }
+  }
+
+  if (jQuery("#woocommerce_wc_openpay_gateway_sandbox").length) {
+    is_sandbox();
+
+    jQuery("#woocommerce_wc_openpay_gateway_sandbox").on(
+      "change",
+      function (e) {
         is_sandbox();
+      },
+    );
+  }
 
-        jQuery("#woocommerce_wc_openpay_gateway_sandbox").on("change", function(e){
-            is_sandbox();
-        });
+  function is_sandbox() {
+    sandbox = jQuery("#woocommerce_wc_openpay_gateway_sandbox").is(":checked");
+    if (sandbox) {
+      jQuery("input[name*='live']").parent().parent().parent().hide();
+      jQuery("input[name*='test']").parent().parent().parent().show();
+    } else {
+      jQuery("input[name*='test']").parent().parent().parent().hide();
+      jQuery("input[name*='live']").parent().parent().parent().show();
     }
-
-    function is_sandbox(){
-        sandbox = jQuery("#woocommerce_wc_openpay_gateway_sandbox").is(':checked');
-        if(sandbox){
-            jQuery("input[name*='live']").parent().parent().parent().hide();
-            jQuery("input[name*='test']").parent().parent().parent().show();
-        }else{
-            jQuery("input[name*='test']").parent().parent().parent().hide();
-            jQuery("input[name*='live']").parent().parent().parent().show();
-        }
-    }
+  }
 });
--- blocks/checkout-form/src/index.js
+++ blocks/checkout-form/src/index.js
@@ -1,15 +1,15 @@
 //import { registerPaymentMethod } from '@woocommerce/blocks-registry';
-import { decodeEntities } from '@wordpress/html-entities';
-import Form from './form';
+import { decodeEntities } from "@wordpress/html-entities";
+import Form from "./form";
+import "./impoconsumo-sumary";
 
 const { registerPaymentMethod } = window.wc.wcBlocksRegistry;
-const { getSetting } = window.wc.wcSettings
+const { getSetting } = window.wc.wcSettings;
 
-const settings = getSetting( 'wc_openpay_gateway_data', {} )
+const settings = getSetting("wc_openpay_gateway_data", {});
 
 //const label = decodeEntities( settings.title )
-const label = decodeEntities( 'Pago con tarjeta (Openpay)' )
-
+const label = decodeEntities("Pago con tarjeta (Openpay)");
 
 /*
 const Content = () => {
@@ -17,19 +17,19 @@ const Content = () => {
 }
 */
 
-const Label = ( props ) => {
-	const { PaymentMethodLabel } = props.components
-	return <PaymentMethodLabel text={ label } />
-}
+const Label = (props) => {
+  const { PaymentMethodLabel } = props.components;
+  return <PaymentMethodLabel text={label} />;
+};
 
-registerPaymentMethod( {
-	name: "wc_openpay_gateway",
-	label: <Label />,
-	content:<Form/>,
-	edit:<Form/>,
-	canMakePayment: () => true,
-	ariaLabel: label,
-	supports: {
-		features: settings.supports,
-	}
-} )
+registerPaymentMethod({
+  name: "wc_openpay_gateway",
+  label: <Label />,
+  content: <Form />,
+  edit: <Form />,
+  canMakePayment: () => true,
+  ariaLabel: label,
+  supports: {
+    features: settings.supports,
+  },
+});
